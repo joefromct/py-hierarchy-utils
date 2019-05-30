ab283fd04583cb0d593bb79a5e24a2ee83c21efb
joefromct <joefromct@fastmail.com>
2019-05-29 23:52:24 -0500

    blacken-mode formatted.

--- hierarchy_utils/main.py
+++ hierarchy_utils/main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 from functools import reduce
 
-def is_valid_hp(hp:str ) -> bool :
-    assert isinstance(hp, str), "The input type for a hp (hierarchy path) must be a string."
-    #assert hp[0] == "/", "A hp (hierarchy path) should start with '/' at a root."
+
+def is_valid_hp(hp: str) -> bool:
+    assert isinstance(
+        hp, str
+    ), "The input type for a hp (hierarchy path) must be a string."
+    # assert hp[0] == "/", "A hp (hierarchy path) should start with '/' at a root."
     return True
 
 
@@ -38,15 +41,15 @@ def list_to_hp(li: list) -> str:
     return "/" + "/".join([str(x) for x in li])
 
 
-def hp_to_list(hp: str) -> list :
+def hp_to_list(hp: str) -> list:
     """Converts a hp (hieriarchy path) to a list (lists are used for toolz's
     get_in, assoc_in, updated_in, etc.).
 
-    Input: ['root', 0, 'thing',2]
-    Output:   /root/0/thing/2
+    Input: /root/0/thing/2
+    Output: ['root', 0, 'thing',2]
 
     """
-    #assert hp[0] == '/', "Hierarchy path must start with a '/' (from root)?"
+    # assert hp[0] == '/', "Hierarchy path must start with a '/' (from root)?"
     is_valid_hp(hp)
     li = [maybe_int(p) for p in hp.split("/") if p != ""]
     return li
@@ -54,7 +57,7 @@ def hp_to_list(hp: str) -> list :
 
 def is_wildcard_hp(hp: str) -> bool:
     "Predicate for If path string has a wildcard char someplace in it's path."
-    #assert hp[0] == '/', "Hierarchy path must start with a '/' (from root)?"
+    # assert hp[0] == '/', "Hierarchy path must start with a '/' (from root)?"
     is_valid_hp(hp)
     if "/*/" in hp:
         return True
@@ -65,7 +68,7 @@ def is_wildcard_hp(hp: str) -> bool:
 def flatten_recur(recursive_lists: list) -> list:
     "Utility function to flatten many nested recursive lists."
     assert isinstance(recursive_lists, list)
-    if recursive_lists == []: # done
+    if recursive_lists == []:  # done
         return recursive_lists
 
     first, *rest = recursive_lists
@@ -76,7 +79,7 @@ def flatten_recur(recursive_lists: list) -> list:
         return [first] + flatten_recur(rest)
 
 
-def get_in_hp(hp: str, d:dict, default=None, no_default=False):
+def get_in_hp(hp: str, d: dict, default=None, no_default=False):
     """Toolz's `get_in` function takes a tuple and a dict, and returns the value
     at that place in the dict.
 
@@ -89,12 +92,10 @@ def get_in_hp(hp: str, d:dict, default=None, no_default=False):
     See test_get_in_hp_usage() for usage examples.
     """
     is_valid_hp(hp)
-    if not is_wildcard_hp(hp): # easy
+    if not is_wildcard_hp(hp):  # easy
         path_list = hp_to_list(hp)
         try:
-            return reduce(lambda x,y: x.__getitem__(y),
-                        path_list,
-                        d)
+            return reduce(lambda x, y: x.__getitem__(y), path_list, d)
         except (KeyError, IndexError, TypeError):
             if no_default:
                 raise
@@ -102,7 +103,9 @@ def get_in_hp(hp: str, d:dict, default=None, no_default=False):
     else:
         parent_path, rest_path = hp.split("/*/", 1)
         child_data = get_in_hp(parent_path, d)
-        assert isinstance(child_data, (list, type(None))), "Wildcard's need lists as children?"
+        assert isinstance(
+            child_data, (list, type(None))
+        ), "Wildcard's need lists as children?"
 
         if child_data:
             return [get_in_hp(rest_path, x) for x in child_data]
@@ -110,16 +113,17 @@ def get_in_hp(hp: str, d:dict, default=None, no_default=False):
 
 def assoc_in_coll(d: dict, coll: list, value, factory=dict) -> dict:
     "Same idea as assoc_in, however uses `coll` similarly to toolz.get_in()."
-    assert len(coll) >= 1, "we should have at least one collection key to get and one collection key to set."
+    assert (
+        len(coll) >= 1
+    ), "we should have at least one collection key to get and one collection key to set."
 
     d2 = factory()
     d2.update(d)
 
     *getters, setter = coll
     # this mutates d2
-    reduce(lambda x,y: x.__getitem__(y),
-                 getters,
-                 d2).__setitem__(setter, value)
+    # TODO Will raise a KeyError if path doesn't exist. Do i want that?
+    reduce(lambda x, y: x.__getitem__(y), getters, d2).__setitem__(setter, value)
     return d2
 
 
@@ -131,18 +135,22 @@ def assoc_in_hp(d: dict, hp: str, value, factory=dict) -> dict:
     return assoc_in_coll(d, coll, value, factory)
 
 
-def explode_path(hp: str, d:dict, parent=None):
-   is_valid_hp(hp)
-   if not is_wildcard_hp(hp):
-       # if it's not a wildcard hp, actually we can just return 'it' (there is only one.)
-       return [hp]
-   else:
+def explode_path(hp: str, d: dict, parent=None):
+    is_valid_hp(hp)
+    if not is_wildcard_hp(hp):
+        # if it's not a wildcard hp, actually we can just return 'it' (there is only one.)
+        return [hp]
+    else:
         parent_path, rest_path = hp.split("/*/", 1)
         child_data = get_in_hp(parent_path, d)
-        assert isinstance(child_data, (list, type(None))), "Wildcard's need lists as children?"
+        assert isinstance(
+            child_data, (list, type(None))
+        ), "Wildcard's need lists as children?"
         if child_data:
-            return [explode_path("/".join([parent_path, str(i), rest_path]), d)
-               for i, _ in enumerate(child_data)]
+            return [
+                explode_path("/".join([parent_path, str(i), rest_path]), d)
+                for i, _ in enumerate(child_data)
+            ]
 
 
 def update_in_hp(d, hp, func, default=None, factory=dict):
@@ -150,7 +158,7 @@ def update_in_hp(d, hp, func, default=None, factory=dict):
 
     def __update_in_hp(d, hp):
         v = func(get_in_hp(hp, d, default=default))
-        return assoc_in_hp(d, hp, v, factory=factory )
+        return assoc_in_hp(d, hp, v, factory=factory)
 
     if not is_wildcard_hp(hp):
         return __update_in_hp(d, hp)
@@ -161,8 +169,3 @@ def update_in_hp(d, hp, func, default=None, factory=dict):
         # reduce across the paths on the input dict.
         r = reduce(__update_in_hp, hps, d)
         return r
-
-
-# reduce takes fn, sequence, and starting
-
-
